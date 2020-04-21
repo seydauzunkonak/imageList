3f4acafd34b2d2929679bbbcc32375476b081503
seydauzunkonak <52824376+seydauzunkonak@users.noreply.github.com>
2020-04-22 02:53:53 +0300

    api

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
 import SearchBar from "./searchBar/searchBar";
+import axios from "axios";
+import ImageList from "./image/imageList";
 import "./app.css";
 
 class App extends Component {
+  state = {
+    images: [],
+  };
   //search fonksiyonu oluşturuldu
-  onSearchImage(search) {
-    console.log("App : " + search);
-  }
+  onSearchImage = async (search) => {
+    const result = await axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: search,
+      },
+      headers: {
+        //kim olduğumuzu anlaması için key gönderme
+        Authorization: "Client-ID Qpy8g9pgEDsPUuK8TM_LdsCYkohQG0bxN5JWFxxlMlQ",
+      },
+    });
+    console.log(result.data.results);
+    this.setState({
+      images: result.data.results,
+    });
+  };
   render() {
     return (
       <div className="app-container">
         <SearchBar onSearchImage={this.onSearchImage} />
+        <ImageList />
       </div>
     );
   }
---
+++ src/components/image/imageList.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const ImageList = () => {
+  return <div>Resim Listesi</div>;
+};
+export default ImageList;
